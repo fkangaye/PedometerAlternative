342249ff21e7a7d6a0c74d0730e4b425a2572033
fkangaye <fkangaye@yahoo.com>
2016-05-04 22:57:16 -0700

    initian commit

---
+++ app/src/main/java/com/example/student/pedometer/MainActivity.java
@@ -0,0 +1,159 @@
+package com.example.student.pedometer;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private boolean mInitialized;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private final float NOISE = (float) 2.0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mInitialized = false;
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        startSensor();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void startSensor() {
+        mSensorManager.registerListener((SensorEventListener) this, mAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        int stepsCount = 0;
+        double mLastX = 0.0;
+        double mLastY = 0.0;
+        double mLastZ = 0.0;
+        // event object contains values of acceleration, read those
+        double x;
+        double y;
+        double z;
+
+        final double alpha = 0.8; // constant for our filter below
+
+        double[] gravity = {0, 0, 0};
+
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+// Remove the gravity contribution with the high-pass filter.
+        x = event.values[0] - gravity[0];
+        y = event.values[1] - gravity[1];
+        z = event.values[2] - gravity[2];
+
+        if (!mInitialized) {
+            // sensor is used for the first time, initialize the last read values
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+            mInitialized = true;
+        } else {
+            // sensor is already initialized, and we have previously read values.
+            // take difference of past and current values and decide which
+            // axis acceleration was detected by comparing values
+
+            double deltaX = Math.abs(mLastX - x);
+            double deltaY = Math.abs(mLastY - y);
+            double deltaZ = Math.abs(mLastZ - z);
+            if (deltaX < NOISE)
+                deltaX = (float) 0.0;
+            if (deltaY < NOISE)
+                deltaY = (float) 0.0;
+            if (deltaZ < NOISE)
+                deltaZ = (float) 0.0;
+            mLastX = x;
+            mLastY = y;
+            mLastZ = z;
+
+            if (deltaX > deltaY) {
+                // Horizontal shake
+                // do something here if you like
+
+            } else if (deltaY > deltaX) {
+                // Vertical shake
+                // do something here if you like
+
+            } else if ((deltaZ > deltaX) && (deltaZ > deltaY)) {
+                // Z shake
+                stepsCount = stepsCount + 1;
+                if (stepsCount > 0) {
+                    TextView textView = (TextView) findViewById(R.id.txtCount);
+                    //Log.i("textView","textView"+R.id.txtCount);
+
+                   textView.setText(getText(stepsCount));
+                }
+
+// Just for indication purpose, I have added vibrate function
+//                // whenever our count moves past multiple of 10
+//                if ((stepsCount % 10) == 0) {
+//                    Util.Vibrate(this, 100);
+//                }
+//            } else {
+//                // no shake detected
+//            }
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //test
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener((SensorEventListener) this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+}
+
